42471a65e0cb1878ca5011da7d298cf64b66077f
Steven Lang <steven.lang.mz@gmail.com>
2019-01-09 22:23:43 +0100

    Update dotfiles

--- scripts/setup-config-symlinks.sh
+++ scripts/setup-config-symlinks.sh
@@ -23,7 +23,7 @@ function link {
 	from=$2
 	to=$3
 
-	if [ -e $1 ]; then
+	if [ -e "$3" ]; then
 		echo "Existing $1 config found. Creating backup with .bak extension ..."
 		mv $3 $3.bak
 	fi
@@ -32,10 +32,12 @@ function link {
 	echo "$2 -> $3"
 }
 
+dotfiles_dir="~/dotfiles"
+
 # Setup links 
-link "zsh" ../config/zsh/.zshrc ~/.zshrc
-link "i3" ../config/i3 ~/.config/i3
-link "i3status" ../config/i3status ~/.config/i3status
-link "vim" ../config/vim/.vimrc ~/.vimrc
-link "polybar" ../config/polybar ~/.config/polybar
-link "flake8" ../config/flake/flake8 ~/.flake8
+link "zsh" $dotfiles_dir/config/zsh/.zshrc ~/.zshrc
+link "i3" $dotfiles_dir/config/i3 ~/.config/i3
+link "i3status" $dotfiles_dir/config/i3status ~/.config/i3status
+link "vim" $dotfiles_dir/config/vim/.vimrc ~/.vimrc
+link "polybar" $dotfiles_dir/config/polybar ~/.config/polybar
+link "flake8" $dotfiles_dir/config/flake/flake8 ~/.flake8
