ad5bffad705ab05b020ca552b140ee382a2bd496
Steven Lang <steven.lang.mz@gmail.com>
2020-12-10 18:54:29 +0100

    Update
    Signed-off-by: Steven Lang <steven.lang.mz@gmail.com>
    
    Former-commit-id: 7c94da59faa15287e182947cc8888bfbf9202db5

--- configs/emacs/.doom.d/custom.el
+++ configs/emacs/.doom.d/custom.el
@@ -3,6 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e8007dd686728ff3d166c3e108138c6b3c9d53242cace89056484419179bcd03" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "ecccd7be38e9ec407473adc315936751b22570b72a6d4db8f3bc9f52dea1f53f" default))
+ '(fci-rule-color "#5B6268")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
+ '(objed-cursor-color "#ff6c6b")
+ '(pdf-view-midnight-colors (cons "#bbc2cf" "#282c34"))
+ '(rustic-ansi-faces
+   ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
  '(safe-local-variable-values
    '((git-commit-major-mode . git-commit-elisp-text-mode)
      (TeX-master . "~/master-thesis/thesis/main.tex")
@@ -12,4 +22,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(doom-modeline-debug ((t (:inherit font-lock-doc-face :slant normal))))
+ '(doom-modeline-evil-emacs-state ((t (:inherit font-lock-builtin-face))))
+ '(doom-modeline-evil-insert-state ((t (:inherit font-lock-keyword-face))))
+ '(doom-modeline-info ((t (:inherit success))))
+ '(doom-modeline-warning ((t (:inherit warning))))
+ '(helm-buffer-modified ((t (:inherit font-lock-comment-face :foreground "IndianRed2"))))
+ '(org-agenda-structure ((t (:height 1.5 :weight ultra-bold :box (:line-width 2 :color "grey75" :style released-button)))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.3))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.25))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.15))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.1))))
+ '(org-super-agenda-header ((t (:height 0.8 :box nil :inherit (org-agenda-structure))))))
