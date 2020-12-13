3102c85cb9ab53b5568891f620aa3e361bbf4a42
Steven Lang <steven.lang.mz@gmail.com>
2020-12-13 12:55:53 +0100

    Update
    Signed-off-by: Steven Lang <steven.lang.mz@gmail.com>
    
    Former-commit-id: 1a1d2f68d436fcd4e2d72d355962b5c95cbe1a89

--- configs/emacs/.doom.d/keybindings.el
+++ configs/emacs/.doom.d/keybindings.el
@@ -57,8 +57,8 @@
       :leader (:prefix "b" :desc "Messages" "s" #'(lambda () (interactive) (switch-to-buffer "*scratch*")))
 
       :leader (:prefix "b" "b" nil)
-      ;; :leader (:prefix "b" :desc "Buffers" "b" #'helm-mini)
-      :leader (:prefix "b" :desc "Buffers" "b" #'+ivy/switch-buffer))
+      :leader (:prefix "b" :desc "Workspace Buffers" "b" #'+ivy/switch-workspace-buffer)
+      :leader (:prefix "b" :desc "All Buffers" "B" #'+ivy/switch-buffer))
 
 
 ;; Windows
@@ -170,7 +170,7 @@
  (:prefix ("s" .  "jump")
   :nv "s"   #'+evil:swiper
   :nv "j"   #'avy-goto-line-below
-  :nv "k"   #'avy-goto-line-below
+  :nv "k"   #'avy-goto-line-above
   :nv "w"   #'avy-goto-word-0
   :nv "c"   #'avy-goto-char
   :nv "t"   #'avy-goto-char-timer))
@@ -221,127 +221,127 @@
 
       ;; Clock
       (:prefix ("C" . "Clock")
-        "c" #'org-clock-cancel
-        "d" #'org-clock-display
-        "e" #'org-evaluate-time-range
-        "g" #'org-clock-goto
-        "i" #'org-clock-in
-        "I" #'org-clock-in-last
-        "o" #'org-clock-out
-        "R" #'org-clock-report
-        "r" #'org-resolve-clocks)
+       "c" #'org-clock-cancel
+       "d" #'org-clock-display
+       "e" #'org-evaluate-time-range
+       "g" #'org-clock-goto
+       "i" #'org-clock-in
+       "I" #'org-clock-in-last
+       "o" #'org-clock-out
+       "R" #'org-clock-report
+       "r" #'org-resolve-clocks)
 
       (:prefix ("d" . "Deadline")
-        "d" #'org-deadline
-        "s" #'org-schedule
-        "t" #'org-time-stamp
-        "T" #'org-time-stamp-inactive)
+       "d" #'org-deadline
+       "s" #'org-schedule
+       "t" #'org-time-stamp
+       "T" #'org-time-stamp-inactive)
 
 
       (:prefix ("f" . "Feed")
-        "i" #'org-feed-goto-inbox
-        "u" #'org-feed-update-all)
+       "i" #'org-feed-goto-inbox
+       "u" #'org-feed-update-all)
 
       (:prefix ("T" . "Toggle")
-        "c" #'org-toggle-checkbox
-        "e" #'org-toggle-pretty-entities
-        "i" #'org-toggle-inline-images
-        "l" #'org-toggle-link-display
-        "t" #'org-show-todo-tree
-        "T" #'org-todo
-        "V" #'space-doc-mode
-        "x" #'org-toggle-latex-fragment)
+       "c" #'org-toggle-checkbox
+       "e" #'org-toggle-pretty-entities
+       "i" #'org-toggle-inline-images
+       "l" #'org-toggle-link-display
+       "t" #'org-show-todo-tree
+       "T" #'org-todo
+       "V" #'space-doc-mode
+       "x" #'org-toggle-latex-fragment)
 
       (:prefix ("s" . "Subtree")
-        "sa" #'org-toggle-archive-tag
-        "A" #'org-archive-subtree
-        "b" #'org-tree-to-indirect-buffer
-        "d" #'org-cut-subtree
-        "h" #'org-promote-subtree
-        "j" #'org-move-subtree-down
-        "k" #'org-move-subtree-up
-        "l" #'org-demote-subtree
-        "n" #'org-narrow-to-subtree
-        "N" #'widen
-        "r" #'org-refile
-        "s" #'org-sparse-tree
-        "S" #'org-sort)
+       "sa" #'org-toggle-archive-tag
+       "A" #'org-archive-subtree
+       "b" #'org-tree-to-indirect-buffer
+       "d" #'org-cut-subtree
+       "h" #'org-promote-subtree
+       "j" #'org-move-subtree-down
+       "k" #'org-move-subtree-up
+       "l" #'org-demote-subtree
+       "n" #'org-narrow-to-subtree
+       "N" #'widen
+       "r" #'org-refile
+       "s" #'org-sparse-tree
+       "S" #'org-sort)
 
       ;; tables
       (:prefix ("t" . "Tables")
-        "a" #'org-table-align
-        "b" #'org-table-blank-field
-        "c" #'org-table-convert
-        "dc" #'org-table-delete-column
-        "dr" #'org-table-kill-row
-        "e" #'org-table-eval-formula
-        "E" #'org-table-export
-        "f" #'org-table-field-info
-        "h" #'org-table-previous-field
-        "H" #'org-table-move-column-left
-        "w" #'org-table-wrap-region
+       "a" #'org-table-align
+       "b" #'org-table-blank-field
+       "c" #'org-table-convert
+       "dc" #'org-table-delete-column
+       "dr" #'org-table-kill-row
+       "e" #'org-table-eval-formula
+       "E" #'org-table-export
+       "f" #'org-table-field-info
+       "h" #'org-table-previous-field
+       "H" #'org-table-move-column-left
+       "w" #'org-table-wrap-region
 
-        ;; Table-insert
-        (:prefix ("i" . "Insert")
-          "c" #'org-table-insert-column
-          "h" #'org-table-insert-hline
-          "H" #'org-table-hline-and-move
-          "r" #'org-table-insert-row
-          "I" #'org-table-import
-          "j" #'org-table-next-row
-          "J" #'org-table-move-row-down
-          "K" #'org-table-move-row-up
-          "l" #'org-table-next-field
-          "L" #'org-table-move-column-right
-          "n" #'org-table-create
-          "N" #'org-table-create-with-table.el
-          "r" #'org-table-recalculate
-          "s" #'org-table-sort-lines)
+       ;; Table-insert
+       (:prefix ("i" . "Insert")
+        "c" #'org-table-insert-column
+        "h" #'org-table-insert-hline
+        "H" #'org-table-hline-and-move
+        "r" #'org-table-insert-row
+        "I" #'org-table-import
+        "j" #'org-table-next-row
+        "J" #'org-table-move-row-down
+        "K" #'org-table-move-row-up
+        "l" #'org-table-next-field
+        "L" #'org-table-move-column-right
+        "n" #'org-table-create
+        "N" #'org-table-create-with-table.el
+        "r" #'org-table-recalculate
+        "s" #'org-table-sort-lines)
 
-        ;; Table-toggle
-        (:prefix ("T" . "Toggle")
-          "tf" #'org-table-toggle-formula-debugger
-          "to" #'org-table-toggle-coordinate-overlays))
+       ;; Table-toggle
+       (:prefix ("T" . "Toggle")
+        "tf" #'org-table-toggle-formula-debugger
+        "to" #'org-table-toggle-coordinate-overlays))
 
       ;; Source blocks / org-babel
       (:prefix ("b" . "SRC")
-        "p"     #'org-babel-previous-src-block
-        "n"     #'org-babel-next-src-block
-        "e"     #'org-babel-execute-maybe
-        "o"     #'org-babel-open-src-block-result
-        "v"     #'org-babel-expand-src-block
-        "u"     #'org-babel-goto-src-block-head
-        "g"     #'org-babel-goto-named-src-block
-        "r"     #'org-babel-goto-named-result
-        "b"     #'org-babel-execute-buffer
-        "s"     #'org-babel-execute-subtree
-        "d"     #'org-babel-demarcate-block
-        "t"     #'org-babel-tangle
-        "f"     #'org-babel-tangle-file
-        "c"     #'org-babel-check-src-block
-        "j"     #'org-babel-insert-header-arg
-        "l"     #'org-babel-load-in-session
-        "i"     #'org-babel-lob-ingest
-        "I"     #'org-babel-view-src-block-info
-        "z"     #'org-babel-switch-to-session
-        "Z"     #'org-babel-switch-to-session-with-code
-        "a"     #'org-babel-sha1-hash
-        "x"     #'org-babel-do-key-sequence-in-edit-buffer)
+       "p"     #'org-babel-previous-src-block
+       "n"     #'org-babel-next-src-block
+       "e"     #'org-babel-execute-maybe
+       "o"     #'org-babel-open-src-block-result
+       "v"     #'org-babel-expand-src-block
+       "u"     #'org-babel-goto-src-block-head
+       "g"     #'org-babel-goto-named-src-block
+       "r"     #'org-babel-goto-named-result
+       "b"     #'org-babel-execute-buffer
+       "s"     #'org-babel-execute-subtree
+       "d"     #'org-babel-demarcate-block
+       "t"     #'org-babel-tangle
+       "f"     #'org-babel-tangle-file
+       "c"     #'org-babel-check-src-block
+       "j"     #'org-babel-insert-header-arg
+       "l"     #'org-babel-load-in-session
+       "i"     #'org-babel-lob-ingest
+       "I"     #'org-babel-view-src-block-info
+       "z"     #'org-babel-switch-to-session
+       "Z"     #'org-babel-switch-to-session-with-code
+       "a"     #'org-babel-sha1-hash
+       "x"     #'org-babel-do-key-sequence-in-edit-buffer)
 
       (:prefix ("i" . "Insert")
-        ;; insertion
-        "b" #'org-insert-structure-template
-        "d" #'org-insert-drawer
-        "e" #'org-set-effort
-        "f" #'org-footnote-new
-        "h" #'org-insert-heading
-        "H" #'org-insert-heading-after-current
-        "i" #'org-insert-item
-        "l" #'org-insert-link
-        "n" #'org-add-note
-        "p" #'org-set-property
-        "s" #'org-insert-subheading
-        "t" #'org-set-tags-command))
+       ;; insertion
+       "b" #'org-insert-structure-template
+       "d" #'org-insert-drawer
+       "e" #'org-set-effort
+       "f" #'org-footnote-new
+       "h" #'org-insert-heading
+       "H" #'org-insert-heading-after-current
+       "i" #'org-insert-item
+       "l" #'org-insert-link
+       "n" #'org-add-note
+       "p" #'org-set-property
+       "s" #'org-insert-subheading
+       "t" #'org-set-tags-command))
 
 ;; Org agenda mode
 (map! :after org
