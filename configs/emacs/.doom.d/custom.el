98f74e6c97f1df6a0631a03214bd011e8e3e3735
Steven Lang <steven.lang.mz@gmail.com>
2021-01-27 09:56:57 +0100

    Update dotfiles
    Former-commit-id: aa0257a79791232d60f664f6be64cb45ecb48d51

--- configs/emacs/.doom.d/custom.el
+++ configs/emacs/.doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("8eeb028ce8812246210ff710d017ce6e514cf36f4359424d8b6d7d5a87f5b2af" "e8007dd686728ff3d166c3e108138c6b3c9d53242cace89056484419179bcd03" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "ecccd7be38e9ec407473adc315936751b22570b72a6d4db8f3bc9f52dea1f53f" default))
+   '("7a994c16aa550678846e82edc8c9d6a7d39cc6564baaaacc305a3fdc0bd8725f" "8eeb028ce8812246210ff710d017ce6e514cf36f4359424d8b6d7d5a87f5b2af" "e8007dd686728ff3d166c3e108138c6b3c9d53242cace89056484419179bcd03" "4a8d4375d90a7051115db94ed40e9abb2c0766e80e228ecad60e06b3b397acab" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "ecccd7be38e9ec407473adc315936751b22570b72a6d4db8f3bc9f52dea1f53f" default))
  '(fci-rule-color "#5B6268")
  '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
@@ -14,7 +14,8 @@
  '(rustic-ansi-faces
    ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
  '(safe-local-variable-values
-   '((git-commit-major-mode . git-commit-elisp-text-mode)
+   '((doom-font font-spec :family "DejaVu Sans Mono" :size 25)
+     (git-commit-major-mode . git-commit-elisp-text-mode)
      (TeX-master . "~/master-thesis/thesis/main.tex")
      (projectile-project-root . "~/master-thesis/thesis"))))
 (custom-set-faces
