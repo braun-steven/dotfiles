0df9ac6df8c3be62820ffbcdc12c4511637cc4ac
Steven Lang <steven.lang.mz@gmail.com>
2021-07-12 13:23:47 +0200

    Update
    Signed-off-by: Steven Lang <steven.lang.mz@gmail.com>
    
    Former-commit-id: 639e6f679cae6e45d2df028574d2891e63b537fd

--- configs/fish/.config/fish/config.fish
+++ configs/fish/.config/fish/config.fish
@@ -1,9 +1,23 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # eval (keychain --quiet --agents ssh id_rsa)
+    # eval (keychain -Q --quiet id_rsa)
 end
 
-# Make errors red
+# Make errors bold-red
 set fish_color_error red --bold
+
+# Make menu selection background blue
+set fish_color_search_match --background=blue
 
 # Enable vim mode
 fish_vi_key_bindings
+
+# Set fish done to 30s
+set -U __done_min_cmd_duration 30000  # default: 5000 ms
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/tak/.conda/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
