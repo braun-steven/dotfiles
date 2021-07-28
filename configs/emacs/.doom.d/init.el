eb91e297f081ee7724c89152cae911981e827565
Steven Lang <steven.lang.mz@gmail.com>
2021-07-28 12:09:17 +0200

    Update dotfiles
    Former-commit-id: 213d5d7e45748dd7bf2e370ddf2298f0454bb254

--- configs/emacs/.doom.d/init.el
+++ configs/emacs/.doom.d/init.el
@@ -143,7 +143,7 @@
        ;;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
         +dragndrop       ; drag & drop files/images into org buffers
-        +roam
+        +roam2
         +journal
         ;;+hugo            ; use Emacs for hugo blogging
         ;;+jupyter        ; ipython/jupyter support for babel
