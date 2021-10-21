de22300d15aeb5c9488f799eaccf12a1b9aa8072
Steven Lang <steven.lang.mz@gmail.com>
2021-10-21 17:02:34 +0200

    Update dotfiles
    Former-commit-id: 7aad5c4d1d85c8e9290b7f9ea56ce3e8a5e7ad32

--- configs/emacs/.doom.d/private/lsp/config.el
+++ configs/emacs/.doom.d/private/lsp/config.el
@@ -3,6 +3,7 @@
   (load! "+functions")
   (load! "+keybindings")
 
+
   ;; Ignore __pycache__ directories
   (push "[/\\\\]_pycache_\\'" lsp-file-watch-ignored-directories)
 
@@ -12,4 +13,8 @@
   ;; Set file-watcher higher
   (setq lsp-file-watch-threshold 2000)
   (setq lsp-enable-folding t)
+
+
+  ;; From https://emacs-lsp.github.io/lsp-mode/page/faq/#how-do-i-force-lsp-mode-to-forget-the-workspace-folders-for-multi-root
+  (advice-add 'lsp :before (lambda (&rest _args) (eval '(setf (lsp-session-server-id->folders (lsp-session)) (ht)))))
   )
