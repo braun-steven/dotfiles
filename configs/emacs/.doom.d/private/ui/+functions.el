a83fc33e67edfd63cebf4e6387989f99afd53e31
Steven Lang <steven.lang.mz@gmail.com>
2022-10-06 20:07:21 +0200

    Update
    Signed-off-by: Steven Lang <steven.lang.mz@gmail.com>
    
    Former-commit-id: 20ab7a635b8d6c07858836f440bde919abbd08f9

--- configs/emacs/.doom.d/private/ui/+functions.el
+++ configs/emacs/.doom.d/private/ui/+functions.el
@@ -1,15 +1,17 @@
 ;;; ../dotfiles/configs/emacs/.doom.d/+modules/ui/theme/functions.el -*- lexical-binding: t; -*-
 
-(defun ap/load-doom-theme (theme)
-  "Disable active themes and load a Doom theme."
+(defun slang/load-doom-theme (theme)
+  "Disable active themes and load a Doom theme.
+   Source: https://www.reddit.com/r/emacs/comments/ezetx0/doomthemes_screenshots_updated_good_time_to_go/"
   (interactive (list (intern (completing-read "Theme: "
                                               (->> (custom-available-themes)
                                                    (-map #'symbol-name)
                                                    (--select (string-prefix-p "doom-" it)))))))
-  (ap/switch-theme theme))
+  (ap/load-theme theme))
 
-(defun ap/switch-theme (theme)
-  "Disable active themes and load THEME."
+(defun slang/load-theme (theme)
+  "Disable active themes and load THEME.
+   Source: https://www.reddit.com/r/emacs/comments/ezetx0/doomthemes_screenshots_updated_good_time_to_go/"
   (interactive (list (intern (completing-read "Theme: "
                                               (->> (custom-available-themes)
                                                    (-map #'symbol-name))))))
