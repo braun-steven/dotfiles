3f791eddcb905ba633ea90ff266053565aa34190
Steven Braun <steven.braun.mz@gmail.com>
2022-11-18 10:10:48 +0100

    Update
    Signed-off-by: Steven Braun <steven.braun.mz@gmail.com>
    
    Former-commit-id: 0e5431d2034c160992922d13c4c89ca6438ac962

--- configs/fish/.config/fish/functions/fish_user_key_bindings.fish
+++
@@ -1,8 +0,0 @@
-function fish_user_key_bindings
-    bind -M insert \cp up-or-search
-    bind -M insert \cn down-or-search
-    bind -M insert \cf forward-char
-    set -g fish_key_bindings fish_vi_key_bindings
-end
-
-fzf_key_bindings
