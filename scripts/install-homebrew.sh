103ce946ebb48bdbc7588e78c4342e9d17896ed1
Steven Braun <steven.braun.mz@gmail.com>
2023-02-14 13:47:56 +0100

    Update dotfiles
    Former-commit-id: fb5fcc4b83db517e893abcac19d859649c8fdc8a

--- scripts/install-homebrew.sh
+++ scripts/install-homebrew.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 # From: https://docs.brew.sh/Installation
+cd $HOME
 git clone https://github.com/Homebrew/brew homebrew
 eval "$(homebrew/bin/brew shellenv)"
 brew update --force --quiet
