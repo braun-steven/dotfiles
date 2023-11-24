b42ef104e0f59c6a41139de6aa974582c177e6b2
Steven Braun <steven.braun.mz@gmail.com>
2023-11-24 10:48:31 +0100

    Update
    Former-commit-id: d29a62a47d8480bf4b315dee128dae73898e5e8d

--- configs/sketchybar/.config/sketchybar/items/front_app.sh
+++ configs/sketchybar/.config/sketchybar/items/front_app.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 front_app=(
-  label.font="$FONT:Black:12.0"
+  label.font="$FONT:Black:13.0"
   icon.background.drawing=on
+  icon.background.image.scale=0.75
   display=active
   script="$PLUGIN_DIR/front_app.sh"
   click_script="open -a 'Mission Control'"
 )
 
-sketchybar --add item front_app left         \
+sketchybar --add item front_app center         \
            --set front_app "${front_app[@]}" \
            --subscribe front_app front_app_switched
