2cab188bbb879390b04516642a31d7997066ec64
Steven Braun <steven.braun.mz@gmail.com>
2024-01-26 12:21:45 +0100

    Update dotfiles
    Former-commit-id: cbc3d62699254b82b7ee9a62a4c1c3141a430b5e

---
+++ configs/emacs/.doom.d/private/typst/config.el
@@ -0,0 +1,6 @@
+;;; ../dotfiles/configs/emacs/.doom.d/lang/+latex.el -*- lexical-binding: t; -*-
+(after! typst
+  (load! "+functions")
+  (load! "+keybindings")
+  ;; Necessary or else localleader is not detected
+  (add-hook 'typst-ts-mode-hook #'evil-normalize-keymaps))
