1209110f80c43d07544a7eabf31e488d315c54f8
Steven Braun <steven.braun.mz@gmail.com>
2024-02-19 13:24:55 +0100

    Update

--- configs/emacs/.doom.d/private/org/+org-setup.el
+++ configs/emacs/.doom.d/private/org/+org-setup.el
@@ -10,17 +10,18 @@
 
 (setq org-roam-capture-templates
       '(
-        ("d" "default" plain "%?" :target
-         (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+        ("d" "default" plain "%?"
+         :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
          :unnarrowed t)
         ("m" "meeting" plain (file "~/org/notes/templates/meetings.org")
          :target (file+head "~/org/notes/phd/meetings/misc.org" "#+title: ${title}\n")
          :unnarrowed t)
         ("s" "pathfinder session" plain (file "~/org/notes/templates/pathfinder-session.org")
-         :target (file "~/org/pathfinder-blood-lords/20231124162340-pathfinder_2e_blood_lords_session_notes.org")
-         :jump-to-captured
-         :empty-lines 1
-         :time-prompt
+         ;; :target (file "~/org/pathfinder-blood-lords/20231124162340-pathfinder_2e_blood_lords_session_notes.org")
+         :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+         ;; :jump-to-captured
+         ;; :empty-lines 1
+         ;; :time-prompt
          :unnarrowed t)
         ))
 
