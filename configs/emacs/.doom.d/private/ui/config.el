bf8cac67f09cbe7d255a9b5174971a45bc34ee62
Steven Braun <steven.braun.mz@gmail.com>
2024-03-10 12:58:49 +0100

    Update

--- configs/emacs/.doom.d/private/ui/config.el
+++ configs/emacs/.doom.d/private/ui/config.el
@@ -6,15 +6,15 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
 ;; (setq sbraun/theme-light 'modus-operandi)
-(setq sbraun/theme-dark 'doom-nord)
+(setq sbraun/theme-dark 'doom-one)
 (setq doom-theme sbraun/theme-dark)
 
 ;; Add set-frame-opacity to doom-switch-frame-hook
 ;; Necessary for new emacsclient frames
-(setq! opacity 90)
-(doom/set-frame-opacity opacity)
-(add-hook! 'doom-switch-frame-hook
-  (doom/set-frame-opacity opacity))
+;; (setq! opacity 90)
+;; (doom/set-frame-opacity opacity)
+;; (add-hook! 'doom-switch-frame-hook
+;;   (doom/set-frame-opacity opacity))
 
 (use-package! modus-themes
   :init
