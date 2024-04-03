3d5e7675287dd02a385b59784b6f29e3be7c5217
Steven Braun <steven.braun.mz@gmail.com>
2024-04-03 12:47:55 +0200

    Update dotfiles

--- configs/emacs/.doom.d/private/lsp/config.el
+++ configs/emacs/.doom.d/private/lsp/config.el
@@ -1,5 +1,6 @@
 ;;; ../dotfiles/configs/emacs/.doom.d/tools/+lsp.el -*- lexical-binding: t; -*-
 (after! lsp-mode
+  (add-hook! text-mode '(lambda () (lsp)))
   (load! "+functions")
   (load! "+keybindings")
 
@@ -16,6 +17,16 @@
   (setq lsp-enable-folding t)
 
   (setq lsp-ui-doc-enable nil)
+  ;; (setq lsp-ui-doc-enable t)
+  ;; (setq lsp-ui-doc-show-with-cursor t)
+  ;; (setq lsp-lens-enable t)
+  ;; (setq lsp-headerline-breadcrumb-enable t)
+  ;; (setq lsp-ui-sideline-enable t)
+  ;; (setq lsp-modeline-code-actions-enable t)
+  ;; (setq lsp-signature-auto-activate t)
+  ;; (setq lsp-signature-render-documentation t)
+  ;; (setq lsp-completion-show-detail t)
+  ;; (setq lsp-completion-show-kind t)
 
   (add-hook 'lsp-after-apply-edits-hook
             (lambda (operation)
@@ -28,11 +39,20 @@
   ;; (setq lsp-ui-doc-position 'at-point)
 
 
-  ;; (use-package! lsp-grammarly
-  ;;       :init (load! "keytar")
-  ;;       :hook (markdown-mode . (lambda ()
-  ;;               (require 'lsp-grammarly)
-  ;;               (lsp))))  ; or lsp-deferred
+  (delete 'lsp-terraform lsp-client-packages)
+
+
+(use-package! lsp-grammarly
+  :defer t
+  :hook ((text-mode . lsp)
+         (org-mode . lsp)
+         (markdown-mode . lsp)
+                (gfm-mode . lsp)
+                (TeX-mode . lsp)
+         )
+  :config
+  (add-to-list 'lsp-grammarly-active-modes 'gfm-mode) ;; Add github format markdown mode to grammarly list
+  )
 
   ;; From https://emacs-lsp.github.io/lsp-mode/page/faq/#how-do-i-force-lsp-mode-to-forget-the-workspace-folders-for-multi-root
   ;; (advice-add 'lsp :before (lambda (&rest _args) (eval '(setf (lsp-session-server-id->folders (lsp-session)) (ht)))))
