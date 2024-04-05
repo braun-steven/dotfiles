7265c2bb2b5b2d86cab9dc543ca90842b2d20947
Steven Braun <steven.braun.mz@gmail.com>
2024-04-05 10:38:57 +0200

    Update

---
+++ configs/darkman/.local/share/dark-mode.d/dark.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Sway
+swaymsg "output * bg ~/lakesidedeer-dark.png fill"
+sed -i 's/theme-light/theme-dark/g' ~/.config/sway/config
+swaymsg reload
+
+# Alacritty
+        sed -i 's/github_light/nord/g' ~/.config/alacritty/alacritty.toml
+
+# Vim
+sed -i 's/background=light/background=dark/g' ~/.vimrc
+
+# Gnome
+gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+gsettings set org.gnome.desktop.wm.preferences theme Adwaita-dark
+gsettings set org.gnome.desktop.interface icon-theme Adwaita
+gsettings set org.gnome.desktop.interface cursor-theme Adwaita
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
