b8796a9120d810f72a58cfd0ca0418443e124479
Steven Braun <steven.braun.mz@gmail.com>
2024-04-17 07:48:05 +0200

    Update

--- configs/darkman/.local/share/light-mode.d/light.sh
+++ configs/darkman/.local/share/light-mode.d/light.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
 # Sway
-swaymsg "output * bg ~/lakesidedeer-light.png fill"
-sed -i --follow-symlinks 's/theme-dark/theme-light/g' ~/.config/sway/config
-swaymsg reload
+# sed -i --follow-symlinks 's/theme-dark/theme-light/g' ~/.config/sway/config
+# swaymsg reload
+swaymsg "output * bg ~/lakesidedeer-light.png fill" # Set background after sway reload
 
 # Alacritty
-sed -i --follow-symlinks 's/nord/github_light/g' ~/.config/alacritty/alacritty.toml
+# sed -i --follow-symlinks 's/nord/github_light/g' ~/.config/alacritty/alacritty.toml
 
 # Vim
-sed -i --follow-symlinks 's/background=dark/background=light/g' ~/.vimrc
+# sed -i --follow-symlinks 's/background=dark/background=light/g' ~/.vimrc
 
 # Gnome
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita
