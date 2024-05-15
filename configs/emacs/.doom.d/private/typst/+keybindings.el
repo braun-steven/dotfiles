bad80c97ad3fe344ccdf1589ddd8f91ee5ed67de
Steven Braun <steven.braun.mz@gmail.com>
2024-05-15 08:23:11 +0200

    Update dotfiles

--- configs/emacs/.doom.d/private/typst/+keybindings.el
+++ configs/emacs/.doom.d/private/typst/+keybindings.el
@@ -1,11 +1,13 @@
-;;; ../dotfiles/configs/emacs/.doom.d/+modules/lang/latex/keybindings.el -*- lexical-binding: t; -*-
+;;; ../dotfiles/configs/emacs/.doom.d/+modules/typst/keybindings.el -*- lexical-binding: t; -*-
 
 (map! :map typst-ts-mode-map
 
-      :nv "," nil
+      ;; :nv "," nil
 
       :localleader
 
       :desc "View" "v"     #'typst-ts-mode-preview
       :desc "Watch" "w"     #'typst-ts-mode-watch-toggle
-      :desc "Compile" "c"     #'typst-ts-mode-compile-and-preview)
+      :desc "Compile" "c"     #'typst-ts-mode-compile-and-preview
+      :desc "Format" ","    #'sbraun/typstfmt-current-buffer
+      )
--- configs/emacs/.doom.d/private/ui/config.el
+++ configs/emacs/.doom.d/private/ui/config.el
@@ -2,23 +2,27 @@
 (load! "+functions")
 (load! "+keybindings")
 
+(setq modus-themes-italic-constructs t
+      modus-themes-bold-constructs nil
+      modus-themes-common-palette-overrides '(
+                                        ;; (string green)
+                                        )
+      )
+
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. These are the defaults.
-;; (setq sbraun/theme-light 'modus-operandi)
 (setq sbraun/theme-dark 'doom-nord)
+(setq sbraun/theme-light 'modus-operandi)
 (setq doom-theme sbraun/theme-dark)
 
-;; (set-doom-theme-based-on-file)
-
-;; (use-package! circadian
-;;   :ensure t
-;;   :config
-;;   (setq calendar-latitude 49.992)
-;;   (setq calendar-longitude 8.247)
-;;   (setq circadian-themes '((:sunrise . modus-operandi)
-;;                            (:sunset  . doom-nord)))
-;;   (circadian-setup))
+(use-package! circadian
+  :config
+  (setq calendar-latitude 49.992)
+  (setq calendar-longitude 8.247)
+  (setq circadian-themes `((:sunrise . ,sbraun/theme-light)
+                           (:sunset  . ,sbraun/theme-dark)))
+  (circadian-setup))
 
 
 ;; Add set-frame-opacity to doom-switch-frame-hook
@@ -27,15 +31,3 @@
 ;; (doom/set-frame-opacity opacity)
 ;; (add-hook! 'doom-switch-frame-hook
 ;;   (doom/set-frame-opacity opacity))
-
-(use-package! modus-themes
-  :init
-  ;; Add all your customizations prior to loading the themes
-  (setq modus-themes-italic-constructs t
-        modus-themes-bold-constructs nil
-        modus-themes-common-palette-overrides '(
-                                                ;; (comment yellow-cooler)
-                                                (string green-cooler)
-                                                )
-        )
-  )
