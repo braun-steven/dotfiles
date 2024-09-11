345bd1f4980ed27a3c7b5392d14607efc550db81
Steven Braun <steven.braun.mz@gmail.com>
2024-09-11 13:23:58 +0200

    Update dotfiles

--- configs/emacs/.doom.d/config.el
+++ configs/emacs/.doom.d/config.el
@@ -193,9 +193,9 @@
     (setq corfu-auto-delay 0.0))
 
 ;; Orderless setup
-(use-package! orderless
-    :config
-    (setq completion-styles '(flex orderless basic)))
+;; (use-package! orderless
+;;     :config
+;;     (setq completion-styles '(flex orderless basic)))
 
 
 ;; accept completion from copilot
