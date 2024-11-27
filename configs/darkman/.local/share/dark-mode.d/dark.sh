e64ab357cbd9bb44d3ddaa3f1eff2843824144cf
Steven Braun <steven.braun.mz@gmail.com>
2024-11-27 13:16:46 +0100

    Update dotfiles

--- configs/darkman/.local/share/dark-mode.d/dark.sh
+++ configs/darkman/.local/share/dark-mode.d/dark.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env sh
 
-# Sway
-# sed -i --follow-symlinks 's/theme-light/theme-dark/g' ~/.config/sway/config
-# swaymsg reload
-swaymsg "output * bg ~/wallpaper/macos-big-sur-night.jpg fill" # Set background after sway
+# Symlink sway theme and background (-f overwrites existing symlink)
+ln -f -s ~/.config/sway/theme-dark ~/.config/sway/theme
+ln -f -s ~/.config/sway/background-dark ~/.config/sway/background
+swaymsg reload
 
 # Alacritty
-# sed -i --follow-symlinks 's/github_light/nord/g' ~/.config/alacritty/alacritty.toml
+sed -i --follow-symlinks 's/alabaster/doom_one/g' ~/.config/alacritty/alacritty.toml
 
 # Vim (This is now handled in .vimrc directly)
-# sed -i --follow-symlinks 's/background=light/background=dark/g' ~/.vimrc
+sed -i --follow-symlinks 's/background=light/background=dark/g' ~/.vimrc
 
 # Gnome
-gsettings set org.gnome.desktop.interface gtk-theme Adwaita
-gsettings set org.gnome.desktop.wm.preferences theme Adwaita
+gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+gsettings set org.gnome.desktop.wm.preferences theme Adwaita-dark
 gsettings set org.gnome.desktop.interface icon-theme Adwaita
 gsettings set org.gnome.desktop.interface cursor-theme Adwaita
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
