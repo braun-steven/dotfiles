3c6a6bf0ce010c65c843b2db4a623b562adef723
Steven Braun <steven.braun.mz@gmail.com>
2025-01-27 14:46:49 +0100

    Update dotfiles

--- scripts/rm-symlinks.sh
+++ scripts/rm-symlinks.sh
@@ -1,4 +1,4 @@
-for f in $(stow -n -v -t ~/ * 2>&1 | grep existing | cut -d":" -f 2); do
+for f in $(stow -n -v -t ~/ */ 2>&1 | grep existing | cut -d":" -f 2); do
     target="$HOME/$(echo $f | xargs)"
     if [ -L "$target" ]; then
         echo "$target yes"
