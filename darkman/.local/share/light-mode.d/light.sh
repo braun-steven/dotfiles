9f6af3a5a233ac58975de0c202922cfc749530ba
Steven Braun <steven.braun.mz@gmail.com>
2025-04-09 10:01:22 +0200

    Update

--- darkman/.local/share/light-mode.d/light.sh
+++ darkman/.local/share/light-mode.d/light.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
 # Symlink theme and background (-f overwrites existing symlink)
-ln -f -s ~/.config/sway/background-light ~/.config/sway/background
-ln -f -s ~/.config/sway/theme-light ~/.config/sway/theme
-swaymsg reload
+# ln -f -s ~/.config/sway/background-light ~/.config/sway/background
+# ln -f -s ~/.config/sway/theme-light ~/.config/sway/theme
+# swaymsg reload
 
 # Alacritty
-sed -i --follow-symlinks 's/doom_one/alabaster/g' ~/.config/alacritty/alacritty.toml
+# sed -i --follow-symlinks 's/doom_one/alabaster/g' ~/.config/alacritty/alacritty.toml
 
 # Vim (This is now handled in .vimrc directly)
-sed -i --follow-symlinks 's/background=dark/background=light/g' ~/.vimrc
+# sed -i --follow-symlinks 's/background=dark/background=light/g' ~/.vimrc
 
 # Gnome
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita
