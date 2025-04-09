d036124ed6a0fa3d37be2459ff1ca7991be9621a
Steven Braun <steven.braun.mz@gmail.com>
2025-04-09 11:16:51 +0200

    Update dotfiles

--- darkman/.local/share/dark-mode.d/dark.sh
+++ darkman/.local/share/dark-mode.d/dark.sh
@@ -12,8 +12,14 @@
 # sed -i --follow-symlinks 's/background=light/background=dark/g' ~/.vimrc
 
 # Gnome
-gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
-gsettings set org.gnome.desktop.wm.preferences theme Adwaita-dark
-gsettings set org.gnome.desktop.interface icon-theme Adwaita
+# gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+# gsettings set org.gnome.desktop.wm.preferences theme Adwaita-dark
+# gsettings set org.gnome.desktop.interface icon-theme Adwaita
+# gsettings set org.gnome.desktop.interface cursor-theme Adwaita
+# gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+
+gsettings set org.gnome.desktop.interface gtk-theme Colloid-Dark
+gsettings set org.gnome.desktop.wm.preferences theme Colloid-Dark
+gsettings set org.gnome.desktop.interface icon-theme Colloid
 gsettings set org.gnome.desktop.interface cursor-theme Adwaita
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
